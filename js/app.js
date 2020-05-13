52465b5f1d1a0818d27a0119da3f06eb8bfe96b8
Brunofgm7 <57762223+Brunofgm7@users.noreply.github.com>
2020-05-13 22:42:19 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+
+    burger.addEventListener('click',()=>{
+        //toggle nav
+        nav.classList.toggle('nav-active');
+        //Animate links
+        navLinks.forEach((link, index) =>{
+
+            if(link.style.animation){
+                    link.style.animation = '';
+            } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+
+            //console.log(index);
+            
+            //Delay das letras a aparecer
+            //link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.3}s`;
+            
+            //console.log(index / 7); Delay entre cenas a aparecer de lado
+
+            //Para começar com delay
+            //console.log(index / 7 + 0.2);
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+    
+}
+
+navSlide();
+
+// SE TIVER MTAS FUNCOES
+// const app = () =>{
+//     navSlide();
+// }
