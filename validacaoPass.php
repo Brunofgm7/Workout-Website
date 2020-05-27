fd65cd938e43e022efc38d85fef35742dd1f9018
leonoliveira98 <61556495+leonoliveira98@users.noreply.github.com>
2020-05-27 23:53:03 +0100

    validacoPass + css

--- validacaoPass.php
+++ validacaoPass.php
@@ -1,5 +1,8 @@
 <?php
-    include ('server.php');
+    
+include 'cabecalho.php';
+include 'database.php';
+include 'server.php';
     $db = mysqli_connect('localhost','root','','workout');
     
     $key=$_GET['key'];
@@ -12,22 +15,38 @@
 <body>
 <script src="js/app.js"></script>
 
-    <div>
-        <h2>Nova palavra-passe</h2> 
-        <form action="validacaoPass.php?key=<?=$_SESSION['key']?>" method="post">
-            <?php include('errors.php'); ?>
-            <br>
-            <br>
-            <input type="hidden" name="key" value="<?=$key?>">
-            <label for="NovaPalavraPasse">Inserir nova palavra-passe</label>
-            <input type="text" name="novaPalavraPasse" placeholder="Inserir nova palavra-passe" value="" id="novaPalavraPasse">
-            <br>
-            <br>
-            <label for="RepetirNovaPalavraPasse">Repetir nova palavra-passe</label>
-            <input type="text" name="RepetirNovaPalavraPasse" placeholder="Repetir nova palavra-passe" value="" id="RepetirNovaPalavraPasse">
-            <br>
-            <br>
-            <button type="submit" name="validacaoPass">Guardar nova palavra-passe</button>
-        </form>
+    <div class="validacaoPass">
+        <table>
+            <thead>
+                <h2>Nova palavra-passe</h2> 
+            </thead>
+            <tr>
+                <form action="validacaoPass.php?key=<?=$_SESSION['key']?>" method="post">
+                <?php include('errors.php'); ?>
+                <input type="hidden" name="key" value="<?=$key?>">
+                <td>
+                    <label for="NovaPalavraPasse">Inserir nova palavra-passe</label>
+                </td>
+                <td>
+                    <input class="input2" type="text" name="novaPalavraPasse" placeholder="Inserir nova palavra-passe" value="" id="novaPalavraPasse">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="RepetirNovaPalavraPasse">Repetir nova palavra-passe</label>
+
+                </td>
+                <td>
+                    <input class="input2"type="text" name="RepetirNovaPalavraPasse" placeholder="Repetir nova palavra-passe" value="" id="RepetirNovaPalavraPasse">
+
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button class="validar"type="submit" name="validacaoPass">Guardar nova palavra-passe</button>
+                    </form>
+                </td>
+            </tr>  
+        </table>
     </div>
 </body>
